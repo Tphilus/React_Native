5bd8339ea3f740f64421da57fb060f5f7be0beae
tphilus <tkdadzie@st.ug.edu.gh>
2024-09-28 18:31:18 +0000

    auth screen

---
+++ aora-app/lib/appwrite.js
@@ -0,0 +1,9 @@
+export const appwriteConfig = {
+  endpoint: "https://cloud.appwrite.io/v1",
+  platform: "com.philus.aora",
+  projectId: "66f6d202001fe2845a00",
+  databaseId: "66f6d3fa00346937bd58",
+  userCollectionId: "66f6d4410002a0da5f3f",
+  videoCollectionId: "66f6d4a700285ec4c283",
+  storageId: "66f6d73200033c74ccf8",
+};
