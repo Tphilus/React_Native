22ebecb6b065a63d7d558bff78cd062099e30686
Tphilus <tkanateydadzie@gmail.com>
2025-12-12 16:36:59 +0000

    feat: enhance login screen with social login options and submit button

--- eshop-mobile-app/app/(routes)/login/index.tsx
+++ eshop-mobile-app/app/(routes)/login/index.tsx
@@ -4,6 +4,7 @@ import { router } from "expo-router";
 import React, { useState } from "react";
 import { Controller, useForm } from "react-hook-form";
 import {
+  Image,
   KeyboardAvoidingView,
   Platform,
   ScrollView,
@@ -147,6 +148,8 @@ export default function LoginScreen() {
                 </Text>
               )}
             </View>
+
+            {/* Forgot Password  */}
             <TouchableOpacity
               className="self-end mt-2"
               onPress={() => router.push("/(routes)/forgot-password")}
@@ -157,6 +160,68 @@ export default function LoginScreen() {
               </Text>
             </TouchableOpacity>
           </View>
+
+          {/* Submit Button  */}
+          <TouchableOpacity
+            className={` rounded-xl py-4 mt-8 ${
+              loginForm.formState.isValid ? "bg-blue-600" : "bg-gray-400"
+            }`}
+            // disabled={loginForm.handleSubmit(onlofinForm)}
+            // disabled={!loginForm.formState.isValid || LogingMutation.isValid}
+          >
+            <Text className=" text-white text-center text-lg font-medium">
+              {"Sign In"}
+            </Text>
+          </TouchableOpacity>
+
+          {/* Divider  */}
+          <View className="flex-row items-center my-8">
+            <View className=" flex-1 h-px bg-gray-500 "></View>
+            <Text className=" mx-4 text-gray-500 font-medium text-sm ">
+              Or using other method
+            </Text>
+            <View className=" flex-1 h-px bg-gray-500 "></View>
+          </View>
+
+          {/* Socail Login Buttons  */}
+          <View className=" flex-col gap-4 mb-8 ">
+            {/* Facebook Sing In  */}
+            <TouchableOpacity className=" flex-row items-center justify-center bg-white border border-gray-200 rounded-xl py-4">
+              <View className="w-6 h-6 mr-3">
+                <Image
+                  source={{
+                    uri: "https://developers.google.com/identity/images/g-logo.png",
+                  }}
+                  className="w-full h-full"
+                  resizeMode="contain"
+                />
+              </View>
+              <Text className="text-gray-800 text-base">
+                Sign In with Google?
+              </Text>
+            </TouchableOpacity>
+
+            {/* Facebook Sing In  */}
+            <TouchableOpacity className=" flex-row items-center justify-center bg-white border border-gray-200 rounded-xl py-4">
+              <Ionicons
+                name="logo-facebook"
+                size={24}
+                color="#1877F2"
+                className="mr-3"
+              />
+              <Text className="text-gray-800 text-base">
+                Sign In with Facebook
+              </Text>
+            </TouchableOpacity>
+          </View>
+
+          {/* Switch to Sign Up Links */}
+          <View className="flex-row justify-center mb-8">
+            <Text>Don&apos;t have an account</Text>
+          </View>
+          <TouchableOpacity className="text-blue-600 font-medium text-sm">
+            Sign Up
+          </TouchableOpacity>
         </ScrollView>
       </KeyboardAvoidingView>
     </SafeAreaView>
