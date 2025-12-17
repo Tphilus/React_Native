0822e30108a984cf0bb749543d34b435c2f2dee0
Tphilus <tkanateydadzie@gmail.com>
2025-12-17 04:47:31 +0000

    feat: implement movie fetching and display with loading and error handling

--- mobile_movie_app/app/(tabs)/api.ts
+++ mobile_movie_app/app/(tabs)/api.ts
@@ -3,14 +3,13 @@ export const TMDB_CONFIG = {
   API_KEY: process.env.EXPO_PUBLIC_MOVIE_API_KEY,
   headers: {
     accept: "application/json",
-    Authorization: `Bearer ${process.env.EXPO_PUBLIC_MOVIE_API_KEY}`,
   },
 };
 
 export const fetchMovies = async ({ query }: { query: string }) => {
   const endpoint = query
-    ? `${TMDB_CONFIG.BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
-    : `${TMDB_CONFIG.BASE_URL}/discover/movie?sort_by=popularity.desc`;
+    ? `${TMDB_CONFIG.BASE_URL}/search/movie?query=${encodeURIComponent(query)}&api_key=${TMDB_CONFIG.API_KEY}`
+    : `${TMDB_CONFIG.BASE_URL}/discover/movie?sort_by=popularity.desc&api_key=${TMDB_CONFIG.API_KEY}`;
 
   const response = await fetch(endpoint, {
     method: "GET",
@@ -18,8 +17,7 @@ export const fetchMovies = async ({ query }: { query: string }) => {
   });
 
   if (!response.ok) {
-    // @ts-ignore
-    throw new Error("Failed to fetch movie", response.statusText);
+    throw new Error(`Failed to fetch movie: ${response.statusText}`);
   }
 
   const data = await response.json();
